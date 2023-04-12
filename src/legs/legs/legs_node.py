53f789968509ebf0d9f7c698733d912922006d3d
kylegibson15 <kylegibson15@gmail.com>
2023-04-11 23:21:22 -0600

    rename stand action to movement action

--- src/legs/legs/legs_node.py
+++ src/legs/legs/legs_node.py
@@ -2,23 +2,25 @@ import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
 
-from interfaces.action import Stand
+from interfaces.action import Movement
+
 
 class LegsActionServer(Node):
 
     def __init__(self):
         super().__init__('legs_action_server')
         self._action_server = ActionServer(
-                    self,
-                    Stand,
-                    'movement',
-                    self.execute_callback
-                )
+            self,
+            Movement,
+            'movement',
+            self.execute_callback
+        )
 
     def execute_callback(self, goal_handle):
-        self.get_logger().info('Executing goal...')
-        result = Stand.Result()
+        self.get_logger().info(f'Executing goal... {goal_handle}')
+        result = Movement.Result()
         return result
+
 
 def main(args=None):
     rclpy.init(args=args)
