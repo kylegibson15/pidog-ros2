50c48d76a822be4b0bcda250f1e11ddcc27c84ea
Kyle Gibson <kylegibson15@gmail.com>
2023-04-25 00:12:06 -0600

    clean

--- install/interfaces/share/interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaces_IDL_FILES "action/Movement.idl;srv/ComponentStatus.idl")
-set(interfaces_INTERFACE_FILES "action/Movement.action;srv/ComponentStatus.srv;srv/ComponentStatus_Request.msg;srv/ComponentStatus_Response.msg")
