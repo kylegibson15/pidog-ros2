c595382f72e20d98e46fc2ff1370ba2e1481b635
kylegibson15 <kylegibson15@gmail.com>
2023-05-21 21:27:10 -0600

    bare bones state_publisher

--- src/model/model/state_publisher.py
+++ src/model/model/state_publisher.py
@@ -1,8 +1,8 @@
-from math import sin, cos, pi
+# from math import sin, cos, pi
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
-from geometry_msgs.msg import Quaternion
+# from geometry_msgs.msg import Quaternion
 from sensor_msgs.msg import JointState
 from tf2_ros import TransformBroadcaster, TransformStamped
 
@@ -20,21 +20,21 @@ class StatePublisher(Node):
         self.nodeName = self.get_name()
         self.get_logger().info("{0} started".format(self.nodeName))
 
-        degree = pi / 180.0
+        # degree = pi / 180.0
         loop_rate = self.create_rate(30)
 
         # robot state
-        tilt = 0.
-        tinc = degree
-        swivel = 0.
-        angle = 0.
-        height = 0.
-        hinc = 0.005
+        # tilt = 0.
+        # tinc = degree
+        # swivel = 0.
+        # angle = 0.
+        # height = 0.
+        # hinc = 0.005
 
         # message declarations
-        odom_trans = TransformStamped()
-        odom_trans.header.frame_id = 'odom'
-        odom_trans.child_frame_id = 'axis'
+        # odom_trans = TransformStamped()
+        # odom_trans.header.frame_id = 'odom'
+        # odom_trans.child_frame_id = 'axis'
         joint_state = JointState()
 
         try:
@@ -44,31 +44,31 @@ class StatePublisher(Node):
                 # update joint_state
                 now = self.get_clock().now()
                 joint_state.header.stamp = now.to_msg()
-                joint_state.name = ['swivel', 'tilt', 'periscope']
-                joint_state.position = [swivel, tilt, height]
+                # joint_state.name = ['swivel', 'tilt', 'periscope']
+                # joint_state.position = [swivel, tilt, height]
 
-                # update transform
-                # (moving in a circle with radius=2)
-                odom_trans.header.stamp = now.to_msg()
-                odom_trans.transform.translation.x = cos(angle)*2
-                odom_trans.transform.translation.y = sin(angle)*2
-                odom_trans.transform.translation.z = 0.7
-                odom_trans.transform.rotation = \
-                    euler_to_quaternion(0, 0, angle + pi/2)  # roll,pitch,yaw
+                # # update transform
+                # # (moving in a circle with radius=2)
+                # odom_trans.header.stamp = now.to_msg()
+                # odom_trans.transform.translation.x = cos(angle)*2
+                # odom_trans.transform.translation.y = sin(angle)*2
+                # odom_trans.transform.translation.z = 0.7
+                # odom_trans.transform.rotation = \
+                #     euler_to_quaternion(0, 0, angle + pi/2)  # roll,pitch,yaw
 
-                # send the joint state and transform
+                # # send the joint state and transform
                 self.joint_pub.publish(joint_state)
-                self.broadcaster.sendTransform(odom_trans)
+                # self.broadcaster.sendTransform(odom_trans)
 
-                # Create new robot state
-                tilt += tinc
-                if tilt < -0.5 or tilt > 0.0:
-                    tinc *= -1
-                height += hinc
-                if height > 0.2 or height < 0.0:
-                    hinc *= -1
-                swivel += degree
-                angle += degree/4
+                # # Create new robot state
+                # tilt += tinc
+                # if tilt < -0.5 or tilt > 0.0:
+                #     tinc *= -1
+                # height += hinc
+                # if height > 0.2 or height < 0.0:
+                #     hinc *= -1
+                # swivel += degree
+                # angle += degree/4
 
                 # This will adjust as needed per iteration
                 loop_rate.sleep()
@@ -77,16 +77,16 @@ class StatePublisher(Node):
             pass
 
 
-def euler_to_quaternion(roll, pitch, yaw):
-    qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - \
-        cos(roll/2) * sin(pitch/2) * sin(yaw/2)
-    qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + \
-        sin(roll/2) * cos(pitch/2) * sin(yaw/2)
-    qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - \
-        sin(roll/2) * sin(pitch/2) * cos(yaw/2)
-    qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + \
-        sin(roll/2) * sin(pitch/2) * sin(yaw/2)
-    return Quaternion(x=qx, y=qy, z=qz, w=qw)
+# def euler_to_quaternion(roll, pitch, yaw):
+#     qx = sin(roll/2) * cos(pitch/2) * cos(yaw/2) - \
+#         cos(roll/2) * sin(pitch/2) * sin(yaw/2)
+#     qy = cos(roll/2) * sin(pitch/2) * cos(yaw/2) + \
+#         sin(roll/2) * cos(pitch/2) * sin(yaw/2)
+#     qz = cos(roll/2) * cos(pitch/2) * sin(yaw/2) - \
+#         sin(roll/2) * sin(pitch/2) * cos(yaw/2)
+#     qw = cos(roll/2) * cos(pitch/2) * cos(yaw/2) + \
+#         sin(roll/2) * sin(pitch/2) * sin(yaw/2)
+#     return Quaternion(x=qx, y=qy, z=qz, w=qw)
 
 
 def main():
